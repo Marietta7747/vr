fe0e23ea923e562bb240e901eea3288294c8d78d
Marietta7747 <162296008+Marietta7747@users.noreply.github.com>
2025-02-21 08:24:55 +0100

    Update megalloidok.php

--- megalloidok.php
+++ megalloidok.php
@@ -18,7 +18,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Kaposvár Helyi Járatok</title>
+    <title>KaposTransit</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" />
     <script src="betolt.js"></script>
 
